3c0393ae11c304eb5ba8ba5044405411302eeaed
Aida Harrop <ms.harrop963@gmail.com>
2017-04-29 18:22:45 +0000

    just added in the chart

--- survey.js
+++ survey.js
@@ -20,7 +20,47 @@ $("#surveyform").submit(function(event){
   }
   else{
     $(this).hide();
+      var formanswers=$(":radio:checked").map(
+        function(){
+          return [[
+            this.id,
+            parseInt(this.value)
+          ]]
+          
+        }
+      )
+      .toArray();
+      drawChart(formanswers);
     $("#results").show();
   }
 });
 
+
+  
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+     // google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart(formanswers) {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Questions');
+        data.addColumn('number', 'Score');
+        data.addRows(formanswers);
+          
+
+        // Set chart options
+        var options = {'title':'Food Health'
+                       };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+      
